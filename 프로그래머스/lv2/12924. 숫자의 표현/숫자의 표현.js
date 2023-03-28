4e6aa1dc2027185e591ddbe5507222c839f7647c
조기범 <akfmzh1207@naver.com>
2023-03-28 17:15:35 +0900

    [level 2] Title: 숫자의 표현, Time: 0.27 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    
+    
+   
+    answer = 0
+    for(let i=1; i<=n ; i++){
+        if (n%i===0 &&i%2 ===1){
+            answer+=1
+        }
+    }
+    return answer;
+}
