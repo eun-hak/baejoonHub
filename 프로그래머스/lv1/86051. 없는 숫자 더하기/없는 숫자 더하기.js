f99a1e9583a20ee94bf660e45caf08d610054032
조기범 <akfmzh1207@naver.com>
2023-07-19 21:42:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,15 @@
+function solution(numbers) {
+   let result = 0
+   for (let i =0; i<=9; i++){
+       if(numbers.includes(i))
+       {continue }
+      else
+      {result+=i }
+       
+   }
+    return result
+}
+    
+    
+    
+    
