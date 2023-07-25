e9fc73630d066e737ddb6409435068a0f58672d4
조기범 <akfmzh1207@naver.com>
2023-07-25 11:32:46 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,12 @@
+function solution(s) {
+    if (s.length%2 ===0){
+        let a = Math.floor(s.length/2)
+           return s.substr(a-1,2)
+    }
+    else{
+        let a = Math.floor(s.length/2)
+           return s.substr(a,1)
+    }
+    
+  
+}
