648e128d3067369662a694d6561a2c438176781c
조기범 <akfmzh1207@naver.com>
2023-10-15 18:33:18 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,11 @@
+function solution(array, commands) {
+    let answer = [];
+    for (let i=0;  i<commands.length ; i++){
+        let newArr = array.slice(commands[i][0]-1,commands[i][1]).sort((a,b)=>a-b)       
+        answer.push(newArr[commands[i][2]-1])
+        
+    }
+    
+    
+    return answer;
+}
