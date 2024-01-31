e000192a2c62369dddf481d4f7b249318118680a
JO KIBEOM <akfmzh1207@naver.com>
2024-01-31 14:36:48 +0900

    [Bronze IV] Title: 모음의 개수, Time: 128 ms, Memory: 9496 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.js
@@ -0,0 +1,16 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+for (let ans of input) {
+  const Vowel = ["a", "e", "i", "o", "u"];
+  if (ans === "#") {
+    break;
+  }
+  let answer = 0;
+  for (let i of ans) {
+    if (Vowel.includes(i.toLowerCase())) {
+      answer += 1;
+    }
+  }
+  console.log(answer);
+}
