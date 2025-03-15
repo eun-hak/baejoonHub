84a329a33076792cce74e39aeb7e97665f146c36
JO KIBEOM <akfmzh1207@naver.com>
2025-03-15 14:46:25 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,7 +1,7 @@
 function solution(s) {
     if (s.length%2 ===0){
         let a = s.length/2
-           return s.substr(a-1,2)
+           return s.slice(a-1,a+1)
     }
     else{
         let a = Math.floor(s.length/2)
